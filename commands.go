3e305ca6de880d7a8fcc1a19097bd7a04fb41c20
juandrzej <programming.yippee729@passinbox.com>
2025-11-14 20:03:41 +0100

    plan commands file

--- commands.go
+++ commands.go
@@ -6,13 +6,13 @@ import (
 	"github.com/juandrzej/postgreSQL-blog-aggregator/internal/config"
 )
 
-type state struct {
-	cfg *config.Config
-}
-
 type command struct {
 	name      string
 	arguments []string
+}
+
+type state struct {
+	cfg *config.Config
 }
 
 func handlerLogin(s *state, cmd command) error {
@@ -26,4 +26,16 @@ func handlerLogin(s *state, cmd command) error {
 
 	fmt.Println("The user has been set.")
 	return nil
+}
+
+type commands struct {
+	commands map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
 }
