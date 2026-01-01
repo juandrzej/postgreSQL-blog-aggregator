409e9945595487b8bfdbb08ed0b505c3cda23200
juandrzej <programming.yippee729@passinbox.com>
2026-01-01 19:44:54 +0100

    clean code a bit

---
+++ handlers.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/juandrzej/postgreSQL-blog-aggregator/internal/database"
+)
+
+func handlerLogin(s *state, cmd command) error {
+	// This func logs in given user while checking argument number and throwing error if applicable
+	if len(cmd.arguments) == 0 {
+		return fmt.Errorf("username is required")
+	} else if len(cmd.arguments) > 1 {
+		return fmt.Errorf("too many arguments, only username is required")
+	}
+
+	_, err := s.db.GetUser(context.Background(), cmd.arguments[0])
+	if err == sql.ErrNoRows {
+		return fmt.Errorf("user '%s' doesn't exist in the database", cmd.arguments[0])
+	}
+	if err != nil {
+		return err
+	}
+
+	if err := s.cfg.SetUser(cmd.arguments[0]); err != nil {
+		return err
+	}
+
+	fmt.Println("The user has been set.")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	// This func registers new users in the database
+	if len(cmd.arguments) == 0 {
+		return fmt.Errorf("username is required")
+	} else if len(cmd.arguments) > 1 {
+		return fmt.Errorf("too many arguments, only username is required")
+	}
+
+	_, err := s.db.GetUser(context.Background(), cmd.arguments[0])
+	if err == nil {
+		return fmt.Errorf("user '%s' already exists", cmd.arguments[0])
+	}
+	if err == sql.ErrNoRows {
+	} else {
+		return err
+	}
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.arguments[0],
+	})
+	if err != nil {
+		return err
+	}
+
+	if err := s.cfg.SetUser(cmd.arguments[0]); err != nil {
+		return err
+	}
+	fmt.Println("User was successfully created!")
+	fmt.Println(user)
+
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	// This func deletes all users from the database
+	err := s.db.DeleteAllUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to reset database: %w", err)
+	}
+
+	fmt.Println("Database has been reset successfully!")
+	return nil
+}
+
+func handlerUsers(s *state, cmd command) error {
+	// This func lists all users from the database
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		if user.Name != s.cfg.CurrentUserName {
+			fmt.Printf("* %s\n", user.Name)
+		} else {
+			fmt.Printf("* %s (current)\n", user.Name)
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 )
 
 func main() {
+	// start state and config
 	st := state{}
 	cfg, err := config.Read()
 	if err != nil {
@@ -29,25 +30,27 @@ func main() {
 	dbQueries := database.New(db)
 	st.db = dbQueries
 
+	// register all commands
 	cmds := commands{
 		commands: make(map[string]func(*state, command) error),
 	}
-
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerUsers)
+
+	// arrange arguments given in console
 	arguments := os.Args
 	if len(arguments) < 2 {
 		fmt.Fprintln(os.Stderr, fmt.Errorf("not enough arguments"))
 		os.Exit(1)
 	}
-
 	cmd := command{
 		name:      arguments[1],
 		arguments: arguments[2:],
 	}
 
+	// run command and output any errors
 	err = cmds.run(&st, cmd)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
