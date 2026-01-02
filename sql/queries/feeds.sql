63f07167152f299ddf8fc4a39baf323aa3cd7893
juandrzej <programming.yippee729@passinbox.com>
2026-01-02 23:38:25 +0100

    add feeds schema and feed query creation

---
+++ sql/queries/feeds.sql
@@ -0,0 +1,21 @@
+-- name: CreateFeed :one
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6
+)
+RETURNING *;
+
+-- name: GetUser :one
+-- SELECT * FROM users
+-- WHERE name = $1;
+--
+-- -- name: DeleteAllUsers :exec
+-- DELETE FROM users;
+--
+-- -- name: GetUsers :many
+-- SELECT * FROM users;
