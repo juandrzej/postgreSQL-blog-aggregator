925dc6f88add08b503dfd51e0ad88b77b857e422
juandrzej <programming.yippee729@passinbox.com>
2026-01-06 21:59:39 +0100

    add new migration

--- handlers_feed_follow.go
+++ handlers_feed_follow.go
@@ -10,17 +10,6 @@ import (
 	"github.com/juandrzej/postgreSQL-blog-aggregator/internal/database"
 )
 
-func handlerAgg(s *state, cmd command) error {
-	// This func fetches feed from RSS and prints it to the console
-	feed, err := FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(feed)
-	return nil
-}
-
 func middlewareLoggedIn(
 	handler func(s *state, cmd command, user database.User) error,
 ) func(*state, command) error {
